5ab9e30917ce8c7165f09ff20a8511a76f3d49db
Alexandr Popov <a.popov@yoursolution.ru>
2022-12-23 11:52:58 +0300

    initial commit

---
+++ base/selenium_base.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.remote.webelement import WebElement
+from typing import List
---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options as chrome_options
