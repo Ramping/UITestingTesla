ef9b41bb73e73ec3f074bcc56933da98b8a28147
Alexandr Popov <harvard33rus@gmail.com>
2022-12-29 22:49:42 +0300

    fixes after review

--- pom/buy_car.py
+++ pom/buy_car.py
@@ -5,29 +5,32 @@ from base.selenium_base import SeleniumBase
 from selenium.webdriver.remote.webelement import WebElement
 
 
-class BuyCar(SeleniumBase):
+class BuyCar:
 
     def __init__(self, driver):
-        super().__init__(driver)
         self.driver = driver
+        self.browser = SeleniumBase(self.driver)
 
     def __get_button_menu(self) -> WebElement:
         """Find menu button"""
-        return self.is_visible('css', 'button[special="menuNavItem"]')
+        return self.browser.is_visible('css', 'button[special="menuNavItem"]')
 
     def __get_modelx_in_menu(self) -> WebElement:
         """Find model x link"""
-        return self.is_visible('css', 'a[title="Model X"]')
+        return self.browser.is_visible('css', 'a[title="Model X"]')
 
     def __get_buy_button(self) -> WebElement:
         """Find "Buy Now" button"""
-        return self.is_visible('css', 'div.tcl-badges__button.tcl-animate.tcl-animate--to-reveal.tcl-animate--revealed>a[title="Buy Now"]')
+        return self.browser.is_visible('xpath', "//div[@class='tcl-homepage-hero__content-end']/descendant::section[1]/child::a[@data-gtm-drawer='modelx']/child::span[contains(text(),'Buy Now')]")
 
     def get_no_result_tag(self) -> WebElement:
         """Find no results tag"""
-        return self.is_visible('css', '.no-results-title')
+        return self.browser.is_visible('css', '.no-results-title')
 
     def go_to_modelx(self):
         """Goes to model x section"""
         self.__get_button_menu().click()
         self.__get_modelx_in_menu().click()
+
+    def click_buy_button(self):
+        self.__get_buy_button()
